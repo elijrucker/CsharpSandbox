4c80737e8154555a91d7a6482e7a8e9d9747f725
elijrucker <eli.j.rucker@gmail.com>
2024-09-30 22:36:14 -0500

    Logic with if statements

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,4 +1,7 @@
-﻿// Random dice = new Random();
+﻿
+// Random dice roll
+
+// Random dice = new Random();
 // int roll1 = dice.Next();
 // int roll2 = dice.Next(101);
 // int roll3 = dice.Next(50, 101);
@@ -7,8 +10,50 @@
 // Console.WriteLine($"Second roll: {roll2}");
 // Console.WriteLine($"Third roll: {roll3}");
 
+// ------
+
+
+// Chooses largest number
+
 // int firstValue = 500;
 // int secondValue = 600;
 // int largerValue;
 // largerValue = Math.Max(firstValue, secondValue);
 // Console.WriteLine(largerValue);
+
+// ------
+
+
+// Generates three random numbers and displays them
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total =  roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose");
+}
