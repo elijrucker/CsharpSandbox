77c95b355625ec4bcaad11340285571be1dcbb1d
elijrucker <eli.j.rucker@gmail.com>
2024-10-10 22:41:52 -0500

    Code readability, whitespace and comments part 1

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -137,3 +137,70 @@
 //         Console.WriteLine(orderID);
 //     }
 // }
+
+// ======= GET STARTED WITH CSHARP, PART 2 ============
+/*
+string firstName = "Bob";
+int widgetsPurchased = 7;
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
+*/
+
+
+/*
+    The following code creates five random OrderIDs
+    to test the fraud detection process. OrderIDs
+    consiste of a letter from A to E, and a three
+    digit number. Ex. A123.
+*/
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     int prefixValue = random.Next(65, 70);
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     orderIDs[i] = prefix + suffix;
+// }
+
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// == WHITESPACE EXERCISE ==
+
+// // Example 1 
+// Console
+// . 
+// WriteLine 
+// (
+//     "Hello Example 1!"
+// )
+// ;
+
+// // Example 2
+// string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord +" "+lastWord+"!");
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    } 
+    else 
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fda9aad82b52151b212fe75dce0dfa91e7e409b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+446da7c98e6df58cda6e8480f921456ed2e4ea47")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
