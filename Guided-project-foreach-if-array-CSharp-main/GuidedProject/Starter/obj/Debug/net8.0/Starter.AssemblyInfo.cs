a0a498b2c03f5d1f759110da1d6888459098d073
elijrucker <eli.j.rucker@gmail.com>
2024-10-12 21:59:07 -0500

    Guided Project - Create arrays and foreach loops

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b8cb8cfa631d50bc9b33fc11abdacd4a484f945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee484f36bdeb2bfb987702b89340e615515f5a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
