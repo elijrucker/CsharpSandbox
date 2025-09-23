945c2a4f648e40567b5fb0a614363970f42527ce
Eli Rucker <eli.j.rucker@gmail.com>
2025-09-23 13:55:52 +0000

    Updated menuSelection switch case 2 output

--- guidedProject_conditionalBranching/Program.cs
+++ guidedProject_conditionalBranching/Program.cs
@@ -120,6 +120,11 @@ do
                     petCount += 1;
                 }
             }
+            if (petCount < maxPets)
+            {
+               Console.WriteLine($"We currently have {petCount} " +
+               $"pets that need homes. We can manage {(maxPets - petCount)} more."); 
+            }
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
