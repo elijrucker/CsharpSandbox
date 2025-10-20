a40947696606350dd1889fa49854d95c7cbbc58b
Eli <eli.j.rucker@gmail.com>
2025-10-19 21:02:56 -0500

    Added value vs reference type examples

--- VariableData/obj/Debug/net8.0/VariableData.AssemblyInfo.cs
+++ VariableData/obj/Debug/net8.0/VariableData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VariableData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890aeb74af3b34efb91a70981194b7ead0cc094f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96877b8726458327df604750210f1c795ed8c2b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VariableData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VariableData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
