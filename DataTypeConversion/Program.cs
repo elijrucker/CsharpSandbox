307da50ae37570f3c68c10150a1e910696b80c83
Eli <eli.j.rucker@gmail.com>
2025-10-22 17:26:26 -0500

    Updated starter code, code is error free

--- DataTypeConversion/Program.cs
+++ DataTypeConversion/Program.cs
@@ -88,11 +88,14 @@ decimal value2 = 6.2m;
 float value3 = 4.3f;
 
 // Your code here to set result1
+int result1 = 0;
 // Hint: You need to round the result to nearest integer (don't just truncate)
 Console.WriteLine($"Divide value1 by value2, display the resultas an int: {result1}");
 
 // Your code here to set result2
+decimal result2 = 0;
 Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
 
 // Your code here to set result3
+float result3 = 0;
 Console.WriteLine($"Divide value 3 by value1, display the result as a float: {result3}");
