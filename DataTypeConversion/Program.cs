f8d568f540ca31c0be9501b740d4604b036f7b69
Eli <eli.j.rucker@gmail.com>
2025-10-22 17:24:30 -0500

    Added starter code for unit 6 of 9

--- DataTypeConversion/Program.cs
+++ DataTypeConversion/Program.cs
@@ -60,23 +60,39 @@
 
 // -- UNIT 4 OF 9 --
 
-string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
 
-decimal total = 0m;
-string message = "";
+// decimal total = 0m;
+// string message = "";
 
-foreach (string value in values)
-{
-    decimal number; // stores the TryParse 'out' value
-    if (decimal.TryParse(value, out number))
-    {
-        total += number;
-    }
-    else
-    {
-        message += value;
-    }
-}
+// foreach (string value in values)
+// {
+//     decimal number; // stores the TryParse 'out' value
+//     if (decimal.TryParse(value, out number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         message += value;
+//     }
+// }
 
-Console.WriteLine($"Message: {message}");
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
+
+
+// -- UNIT 6 OF 9 -- Challenge Exercise
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+Console.WriteLine($"Divide value1 by value2, display the resultas an int: {result1}");
+
+// Your code here to set result2
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+Console.WriteLine($"Divide value 3 by value1, display the result as a float: {result3}");
