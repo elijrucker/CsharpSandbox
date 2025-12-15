553263b275c4fb2cba239f99957cb35695c455ea
elijrucker <eli.j.rucker@gmail.com>
2025-12-15 02:39:07 +0100

    Arrays and Helpers 3/10 completed

--- ArraysAndHelpers/Program.cs
+++ ArraysAndHelpers/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] pallets = ["B14", "A11", "B12", "A13"];
+System.Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+System.Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    System.Console.WriteLine($"-- {pallet}");
+}
+
+System.Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+System.Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    System.Console.WriteLine($"-- {pallet}");
+}
+
+System.Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+System.Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    System.Console.WriteLine($"-- {pallet}");
+}
