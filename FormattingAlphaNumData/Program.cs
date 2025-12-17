085278c4e415aff4b52768c16d3e69e490767108
elijrucker <eli.j.rucker@gmail.com>
2025-12-17 16:49:39 +0100

    Updated project to FormattingAlphaNumData

---
+++ FormattingAlphaNumData/Program.cs
@@ -0,0 +1,9 @@
+﻿string first = "Hello";
+string second = "World";
+// string result = string.Format("{0} {1}!", first, second);
+// System.Console.WriteLine(result);
+// System.Console.WriteLine("{1} {0}!", first, second);
+// System.Console.WriteLine("{0} {0} {0}!", first, second);
+System.Console.WriteLine($"{first} {second}!");
+System.Console.WriteLine($"{second} {first}!");
+System.Console.WriteLine($"{first} {first} {first}!");
